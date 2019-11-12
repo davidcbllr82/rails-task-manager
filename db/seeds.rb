281d3a01e0f6a7ab9bce92ea575fe291ad073dfd
David Caballero <david.caballero@code.berlin>
2019-11-12 17:05:28 +0100

    Back end done. Front end in develop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: 'Pay dentist bills', details: 'Need to wire the money for the whitening treatment', completed: false)
+Task.create(title: 'Work on Sieve', details: 'Follow up with the ellaboration of the Wireframe for the user journey', completed: false)
+Task.create(title: 'Fill form', details: 'Career week form from Le Wagon. Fill up by today', completed: false)
+Task.create(title: 'Talk to Mo', details: 'Final details to discuss for the upcoming Christmas', completed: false)
